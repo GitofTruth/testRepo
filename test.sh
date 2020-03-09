ed406af60a80c78ac9d54b48733c610db852902b
Muhammad Faisal <mfaisal@aucegypt.edu>
2020-03-09 22:47:31 +0200

    testing script is now compatiable with the nodejs client package

--- test.sh
+++ test.sh
@@ -1,25 +1,51 @@
-rm -rf testDirectory
+sudo rm -rf testDirectory
 mkdir testDirectory
 cd testDirectory
-
-git clone https://github.com/GitofTruth/testRepo
-mv testRepo repoOne
-
-git clone https://github.com/GitofTruth/testRepo
-mv testRepo repoTwo
-
 mkdir Storage
 
 echo ""
 echo ""
-echo "Starting the initialization procedure of repoOne"
-cd repoOne
-$GOPATH/src/github.com/GitofTruth/GoT/hooks/init-got.sh
-cd ..
+echo "Starting the initialization procedure of repo one"
+mkdir one
+cd one
+git clone https://github.com/GitofTruth/testRepo
+cd testRepo
+got-init
+
+# testing commands for repo one
+git push
+got-client addRepo
+
+echo "hey man" > test.txt
+git add test.txt
+git commit -m "first message from only repo one"
+got-client push
+
+# end of testing commands for repo one
+
+cd ../..
+
+
+
 
 echo ""
 echo ""
-echo "Starting the initialization procedure of repoTwo"
-cd repoTwo
-$GOPATH/src/github.com/GitofTruth/GoT/hooks/init-got.sh
+echo "Starting the initialization procedure of repo two"
+mkdir two
+cd two
+git clone https://github.com/GitofTruth/testRepo
+cd testRepo
+got-init
+
+# testing commands for repo two
+git push
+got-client pull
+ls
+
+# end of testing commands for repo two
+
+cd ../..
+
+
+
 cd ..
