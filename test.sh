9518c35bb7e3a9a7d187c92d6cca1d19667926ca
Muhammad Faisal <mfaisal@aucegypt.edu>
2020-05-07 17:25:09 +0200

    Test for multi users scenario

--- test.sh
+++ test.sh
@@ -1,51 +1,117 @@
-sudo rm -rf testDirectory
-mkdir testDirectory
-cd testDirectory
-mkdir Storage
+# deleting existing directory
+sudo rm -rf directory
 
+mkdir directory
+cd directory
+############################################################
+############################################################
+############################################################
 echo ""
 echo ""
-echo "Starting the initialization procedure of repo one"
-mkdir one
-cd one
-git clone https://github.com/GitofTruth/testRepo
-cd testRepo
+echo "Creating Users for testing"
+mkdir -p owner/testRepo
+cd owner/testRepo
 got-init
+got-client user create owner
+git config --local user.email "owner@got.com"
+git config --local user.name "owner"
+cd ../..
 
-# testing commands for repo one
-git push
-got-client addRepo
+mkdir -p collaboratorOne/testRepo
+cd collaboratorOne/testRepo
+got-init
+got-client user create collaboratorOne
+git config --local user.email "collaboratorOne@got.com"
+git config --local user.name "collaboratorOne"
+cd ../..
 
-echo "hey man" > test.txt
-git add test.txt
-git commit -m "first message from only repo one"
-got-client push
+mkdir -p userOne/testRepo
+cd userOne/testRepo
+got-init
+got-client user create userOne
+git config --local user.email "userOne@got.com"
+git config --local user.name "userOne"
+cd ../..
 
-# end of testing commands for repo one
-
+mkdir -p userTwo/testRepo
+cd userTwo/testRepo
+got-init
+got-client user create userTwo
+git config --local user.email "userTwo@got.com"
+git config --local user.name "userTwo"
 cd ../..
 
 
 
-
+############################################################
+############################################################
+############################################################
 echo ""
 echo ""
-echo "Starting the initialization procedure of repo two"
-mkdir two
-cd two
-git clone https://github.com/GitofTruth/testRepo
-cd testRepo
-got-init
+echo "Starting the initialization procedure of Owner"
+cd owner/testRepo
 
-# testing commands for repo two
-git push
-got-client pull
+echo "cd ../../.. \n source test.sh" > test.sh
+git add test.sh
+git commit -m "first message by the owner"
+
+got-client addRepo
+got-client authorize collaboratorOne collaborator
+got-client queryUsersAccess
+
+cd ../..
+############################################################
+############################################################
+############################################################
+echo ""
+echo ""
+echo "Starting the initialization procedure of collaboratorOne"
+cd collaboratorOne/testRepo
+got-client clone owner testRepo
+
 ls
 
-# end of testing commands for repo two
+got-client authorize userOne read-write
+got-client queryUsersAccess
+
+echo "hey man" > collaboratorOne.txt
+git add collaboratorOne.txt
+git commit -m "first message by collaboratorOne"
+got-client push
 
 cd ../..
+############################################################
+############################################################
+############################################################
+echo ""
+echo ""
+echo "Starting the initialization procedure of userOne"
+cd userOne/testRepo
+got-client clone owner testRepo
 
+ls
 
+got-client authorize userTwo read-write
+got-client queryUsersAccess
 
-cd ..
+echo "hey man" > userOne.txt
+git add userOne.txt
+git commit -m "first message by userOne"
+got-client push
+
+cd ../..
+############################################################
+############################################################
+############################################################
+echo ""
+echo ""
+echo "Starting the initialization procedure of userTwo"
+cd userTwo/testRepo
+got-client clone owner testRepo
+
+ls
+
+cd ../../..
+####################/########################################
+############################################################
+############################################################
